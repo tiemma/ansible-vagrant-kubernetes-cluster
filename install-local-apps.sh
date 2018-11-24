3175f8d79daa0cbefc35d5ddc398f51e978f3f86
blank <bakareemmanuel98@outlook.com>
2018-11-24 18:14:35 +0100

    Added better args for easier local configuration

--- install-local-apps.sh
+++ install-local-apps.sh
@@ -1,7 +1,7 @@
  sudo apt-get update
  sudo apt-get install software-properties-common
  sudo apt-add-repository --yes --update ppa:ansible/ansible
- sudo apt-get install ansible
+ sudo apt-get install -y ansible
 
  # Install the proxy conf [if you're on  a proxy network]
  vagrant plugin install vagrant-proxyconf 
