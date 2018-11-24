353aa3c405cfcd8acf70a17450d82fe06004052a
blank <bakareemmanuel98@outlook.com>
2018-11-24 18:35:21 +0100

    Added script to help debug playbook scripts

---
+++ run-playbook.sh
@@ -0,0 +1,2 @@
+PYTHONUNBUFFERED=1 ANSIBLE_FORCE_COLOR=true ANSIBLE_HOST_KEY_CHECKING=false ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes -o ControlMaster=auto -o ControlPersist=60s' ansible-playbook --connection=ssh --timeout=30 --limit='all' --inventory-file=/home/blank/projects/ansible-kubernetes/.vagrant/provisioners/ansible/inventory -v kube-cluster/$1
+
